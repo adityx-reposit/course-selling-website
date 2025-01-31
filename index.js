e29dcb9c442a75c70c44f93d420626f5845f3d45
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2025-01-31 14:43:12 +0530

    second

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const express=require("express")
 const jwt = require('jsonwebtoken')
 const app=express();
 
+app.use(express.json())
+
+
+app.post("/signup",(req,res)=>{
+
+})
+
+app.post("/login",(req,res)=>{
+
+})
+
+app.post("/seeCourse",(req,res)=>{
+
+})
+app.post("/buyCourse",(req,res)=>{
+
+})
+
+app.post("/purchasedCourse",(req,res)=>{
+
+})
 
 
 
